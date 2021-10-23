fe98a4fd03bc3fb477036df867c4ed4804eafb76
Srikanth_M <srikanthMidathapalli@gmail.com>
2021-10-23 15:26:21 +0530

    finding the occurance of each charecter of the given string using iterative concept

---
+++ OccuranceOfCharInString.java
@@ -0,0 +1,30 @@
+package com.practise.stringprograms;
+public class OccuranceOfCharInString 
+{
+	public static void main(String[] args)
+	{
+		String str="HelloWorld";
+		char []arr=str.toCharArray();
+		int len=arr.length;
+		int count;
+		System.out.println("length of the char array is:"+len);
+		for(int i=0;i<len-1;i++)
+		{
+			count=1;
+			for(int j=i+1;j<len;j++)
+			{
+				if(arr[i]==arr[j]) 
+				{
+					count++;
+					for(int k=j;k<len-1;k++) 
+					{
+						arr[k]=arr[k+1];
+					}
+					len--;
+
+				}
+			}
+				System.out.println(arr[i]+" "+count);	
+	}
+	}
+}
